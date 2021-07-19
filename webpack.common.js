f59a9bc8937d5741e8cd78f9a38b764790976307
Ilya <gamyrkin@web-bee.ru>
2021-07-19 15:54:32 +0300

    feat: Correcting webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: "./src/index.js",
+    profile: "./src/profile.js",
   },
   module: {
     rules: [
