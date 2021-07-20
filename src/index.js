994c08bd1549bccf5b42d1afa57448df993f9637
Ilya <gamyrkin@web-bee.ru>
2021-07-20 22:07:53 +0300

    feat: add docs folder for github pages

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import basicSass from "./styles/basic.sass";
 import headerSass from "./styles/header.sass";
 import profileSass from "./styles/profile.sass";
 
-import profile from "./components/profile";
+import profile from "./components/profile-basic.js";
